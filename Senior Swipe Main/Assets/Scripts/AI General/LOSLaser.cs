bbd226e95727b7faffefa61aa1fb938108a29fcc
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2020-11-05 15:43:06 -0500

    Revert "Added Zambotni and Fire rug relataed aspects"
    This reverts commit d4df08a13202b9bf848a8101f587684971c5a328.

--- Senior Swipe Main/Assets/Scripts/AI General/LOSLaser.cs
+++ Senior Swipe Main/Assets/Scripts/AI General/LOSLaser.cs
@@ -4,33 +4,15 @@ using UnityEngine;
 
 public class LOSLaser : MonoBehaviour
 {
-    [SerializeField] private Transform m_firePoint;
-
-    private LineRenderer m_lineRenderer;
-
-
-    void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        m_lineRenderer = GetComponent<LineRenderer>();
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        m_lineRenderer.SetPosition(0, m_firePoint.position);
-        RaycastHit hit;
-        if (Physics.Raycast(m_firePoint.position, transform.up, out hit, Mathf.Infinity))
-        {
-            if (hit.collider)
-            {
-                m_lineRenderer.SetPosition(1, hit.point);
-                var target = hit.collider.GetComponentInParent<IAffectableByLaserPointer>();
-                target?.LaserEffect();
-                //onLaserHit?.Invoke(hit.point);
-            }
-        }
-        else
-        {
-            m_lineRenderer.SetPosition(1, m_firePoint.up * 99999);
-        }
+        
     }
 }
--- Senior Swipe Main/Assets/Scripts/Gadgets/Slime/SlippySlime.cs
+++ Senior Swipe Main/Assets/Scripts/Gadgets/Slime/SlippySlime.cs
@@ -7,6 +7,7 @@ public class SlippySlime : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Collided");
         var target = other.gameObject.GetComponentInParent<ISlippySlimeEffect>();
         target?.SlippySpeed(2.5f);
     }
