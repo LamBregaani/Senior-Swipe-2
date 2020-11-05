d4df08a13202b9bf848a8101f587684971c5a328
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2020-11-05 15:39:42 -0500

    Added Zambotni and Fire rug relataed aspects

---
+++ Senior Swipe Main/Assets/Scripts/AI General/Behaviours/RoamOnPathBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RoamOnPathBehaviour : AIBehaviour
+{
+    [SerializeField] private Transform[] m_patrolPoints;
+
+    private int m_currentPatrolPointIndex;
+
+    private Transform m_nextPoint;
+
+    private bool m_isActive;
+
+    private NavMeshAgent m_agent;
+
+
+
+    override public void Awake()
+    {
+        GetAIController();
+        m_agent = GetComponentInParent<NavMeshAgent>();
+        if (priority == Priority.Default)
+        {
+            AddToAIController();
+            m_isActive = true;
+        }
+        m_nextPoint = m_patrolPoints[0];
+    }
+
+    private void Update()
+    {
+        CheckBehaviourConditions();
+    }
+
+    override public void Behaviour()
+    {
+        if (!m_agent.pathPending && m_agent.remainingDistance < 0.5f)
+        {
+            if (m_patrolPoints.Length > 0)
+            {
+                m_agent.destination = m_patrolPoints[m_currentPatrolPointIndex].position;
+
+                m_currentPatrolPointIndex++;
+                m_currentPatrolPointIndex %= m_patrolPoints.Length;
+            }
+
+
+        }
+
+
+    }
+
+    override public void CheckBehaviourConditions()
+    {
+
+    }
+}
--- Senior Swipe Main/Assets/Scripts/AI General/LOSLaser.cs
+++ Senior Swipe Main/Assets/Scripts/AI General/LOSLaser.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class LOSLaser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform m_firePoint;
+
+    private LineRenderer m_lineRenderer;
+
+
+    void Awake()
     {
-        
+        m_lineRenderer = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        m_lineRenderer.SetPosition(0, m_firePoint.position);
+        RaycastHit hit;
+        if (Physics.Raycast(m_firePoint.position, transform.up, out hit, Mathf.Infinity))
+        {
+            if (hit.collider)
+            {
+                m_lineRenderer.SetPosition(1, hit.point);
+                var target = hit.collider.GetComponentInParent<IAffectableByLaserPointer>();
+                target?.LaserEffect();
+                //onLaserHit?.Invoke(hit.point);
+            }
+        }
+        else
+        {
+            m_lineRenderer.SetPosition(1, m_firePoint.up * 99999);
+        }
     }
 }
