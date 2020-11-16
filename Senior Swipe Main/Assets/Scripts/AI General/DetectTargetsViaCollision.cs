afbeb746db5916df344d943861a83d1d0d46f964
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2020-11-16 13:23:15 -0500

    Added Hall Way and Sticky Slime level

---
+++ Senior Swipe Main/Assets/Scripts/AI General/DetectTargetsViaCollision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DetectTargetsViaCollision : MonoBehaviour
+{
+
+    private AIState m_state;
+
+    private StoreTarget storeTarget;
+
+    private Collider[] targets;
+
+    [System.Serializable]
+    public class TargetEvent : UnityEvent { }
+
+    [SerializeField] public TargetEvent onTargetFound;
+
+    [SerializeField] public TargetEvent onTargetLost;
+
+    private void Awake()
+    {
+        m_state = GetComponentInChildren<AIState>();
+        storeTarget = GetComponentInChildren<StoreTarget>();
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("collided");
+        storeTarget.target = collision.gameObject;
+        onTargetFound?.Invoke();
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        storeTarget.target = null;
+        onTargetLost?.Invoke();
+    }
+}
--- Senior Swipe Main/Assets/Scripts/Player/Input/TargetRaycast.cs
+++ Senior Swipe Main/Assets/Scripts/Player/Input/TargetRaycast.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class TargetRaycast : MonoBehaviour
 {
+    [SerializeField] private LayerMask mask;
+
     [SerializeField] private Camera testCamera;
 
     private Vector3 hitPosition;
@@ -22,16 +24,25 @@ public class TargetRaycast : MonoBehaviour
 
     private void LateUpdate()
     {
-        Ray ray = m_cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            hitPosition = hit.point;
-            Debug.Log(hitPosition.ToString());
-        }
+        //Ray ray = m_cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //    hitPosition = hit.point;
+        //}
 
+        RaycastHit hit;
+        if (Physics.Raycast(m_cam.transform.position, m_cam.transform.forward, out hit, Mathf.Infinity, mask))
+        {
+            if (hit.collider)
+            {
+                hitPosition = hit.point;
+            }
+        }
         else
             print("I'm looking at nothing!");
+
+        Debug.DrawRay(transform.position, m_cam.transform.forward, Color.green, 100);
     }
 
     public void ChangeCamera(Camera _camera)
